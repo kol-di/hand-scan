9418f2560366ba833929e15c1160e4d845291fef
Dmitry <kol-di@yandex.ru>
2023-04-28 15:24:35 +0300

    Setup to discover project files

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+"""Blender add-on setup"""
+
 bl_info = {
     "name" : "HandScan",
     "author" : "koldi",
@@ -28,3 +30,21 @@ def register():
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
+
+
+""" To run the addon you have to options
+
+1) Install required packages into the directory with Blender python 
+similiar to https://github.com/jkirsons/FacialMotionCapture_v2/blob/master/OpenCVAnimOperator.py
+
+2) Create your own venv and specify path to site-packages folder in SITE_PACKAGES_PATH variable
+"""
+
+import sys
+from pathlib import Path
+
+addon_path = str(Path(bpy.path.abspath(__file__)).parent.absolute())
+sys.path.append(addon_path)
+
+SITE_PACKAGES_PATH = str(Path(__file__).parent / 'venv' / 'Lib' / 'site-packages')
+sys.path.append(SITE_PACKAGES_PATH)
