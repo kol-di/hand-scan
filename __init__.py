66210326138c7b979157ac24f869baa7036295d4
Dmitry <kol-di@yandex.ru>
2023-04-28 20:42:39 +0300

    package discovery order fix

--- __init__.py
+++ __init__.py
@@ -1,3 +1,23 @@
+""" To run the addon you have to options
+
+1) Install required packages into the directory with Blender python 
+similiar to https://github.com/jkirsons/FacialMotionCapture_v2/blob/master/OpenCVAnimOperator.py
+
+2) Create your own venv and specify path to site-packages folder in SITE_PACKAGES_PATH variable
+"""
+
+import sys
+from pathlib import Path
+
+import bpy
+
+addon_path = str(Path(bpy.path.abspath(__file__)).parent.absolute())
+sys.path.append(addon_path)
+
+SITE_PACKAGES_PATH = str(Path(__file__).parent / 'venv' / 'Lib' / 'site-packages')
+sys.path.append(SITE_PACKAGES_PATH)
+
+
 """Blender add-on setup"""
 
 bl_info = {
@@ -11,8 +31,6 @@ bl_info = {
     "category" : "Generic"
 }
 
-
-import bpy
 
 from .addon.operators import HS_OT_CreateLeftHandMesh, HS_OT_CaptureHands
 from .addon.panel import HS_PT_Panel
@@ -30,21 +48,3 @@ def register():
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
-
-
-""" To run the addon you have to options
-
-1) Install required packages into the directory with Blender python 
-similiar to https://github.com/jkirsons/FacialMotionCapture_v2/blob/master/OpenCVAnimOperator.py
-
-2) Create your own venv and specify path to site-packages folder in SITE_PACKAGES_PATH variable
-"""
-
-import sys
-from pathlib import Path
-
-addon_path = str(Path(bpy.path.abspath(__file__)).parent.absolute())
-sys.path.append(addon_path)
-
-SITE_PACKAGES_PATH = str(Path(__file__).parent / 'venv' / 'Lib' / 'site-packages')
-sys.path.append(SITE_PACKAGES_PATH)
